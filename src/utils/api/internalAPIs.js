3e6ea265f3137ae81b43ff5ec1c51f7f70d893ac
MethmaW <methma.cw@gmail.com>
2021-05-10 20:40:25 +0530

    Add heroku url

--- src/utils/api/internalAPIs.js
+++ src/utils/api/internalAPIs.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const getEquipmentData = async (max, last) => {
 	return await axios({
-		url: `${process.env.REACT_APP_BACKEND_URL}/api/dashboard/${max}/${last}`,
+		url: `${process.env.REACT_APP_BACKEND_HOSTED_URL}/api/dashboard/${max}/${last}`,
 		method: "GET",
 	})
 		.then((res) => res.data)
